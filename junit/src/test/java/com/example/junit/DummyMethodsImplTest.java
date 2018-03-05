b89f9b62967d612525f612ba2407a4a9cfbeeb5f
Krisztina Kovacs <krisztina@smartup.io>
2018-03-05 16:01:10 +0200

    Added test class

---
+++ junit/src/test/java/com/example/junit/DummyMethodsImplTest.java
@@ -0,0 +1,12 @@
+package com.example.junit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class DummyMethodsImplTest {
+
+    @Test
+    public void myFirstTest(){
+        assertTrue(1>0);
+    }
+}
